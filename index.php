f5811685490a888d0eb6ca12073d13fe0a9ced21
Palm5 <andrea.cinquepalmi@hotmail.it>
2021-03-08 02:30:14 +0100

    "backend" introdotta

--- index.php
+++ index.php
@@ -1,89 +1,22 @@
 <?php
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
-
-function getOS() { 
-
-    global $user_agent;
-
-    $os_platform  = "Unknown OS Platform";
-
-    $os_array     = array(
-                          '/windows nt 10/i'      =>  'Windows 10',
-                          '/windows nt 6.3/i'     =>  'Windows 8.1',
-                          '/windows nt 6.2/i'     =>  'Windows 8',
-                          '/windows nt 6.1/i'     =>  'Windows 7',
-                          '/windows nt 6.0/i'     =>  'Windows Vista',
-                          '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
-                          '/windows nt 5.1/i'     =>  'Windows XP',
-                          '/windows xp/i'         =>  'Windows XP',
-                          '/windows nt 5.0/i'     =>  'Windows 2000',
-                          '/windows me/i'         =>  'Windows ME',
-                          '/win98/i'              =>  'Windows 98',
-                          '/win95/i'              =>  'Windows 95',
-                          '/win16/i'              =>  'Windows 3.11',
-                          '/macintosh|mac os x/i' =>  'Mac OS X',
-                          '/mac_powerpc/i'        =>  'Mac OS 9',
-                          '/linux/i'              =>  'Linux',
-                          '/ubuntu/i'             =>  'Ubuntu',
-                          '/iphone/i'             =>  'iPhone',
-                          '/ipod/i'               =>  'iPod',
-                          '/ipad/i'               =>  'iPad',
-                          '/android/i'            =>  'Android',
-                          '/blackberry/i'         =>  'BlackBerry',
-                          '/webos/i'              =>  'Mobile'
-                    );
-
-    foreach ($os_array as $regex => $value)
-        if (preg_match($regex, $user_agent))
-            $os_platform = $value;
-
-    return $os_platform;
-}
-
-function getBrowser() {
-
-    global $user_agent;
-
-    $browser        = "Unknown Browser";
-
-    $browser_array = array(
-                            '/msie/i'      => 'Internet Explorer',
-                            '/firefox/i'   => 'Firefox',
-                            '/safari/i'    => 'Safari',
-                            '/chrome/i'    => 'Chrome',
-                            '/edge/i'      => 'Edge',
-                            '/opera/i'     => 'Opera',
-                            '/netscape/i'  => 'Netscape',
-                            '/maxthon/i'   => 'Maxthon',
-                            '/konqueror/i' => 'Konqueror',
-                            '/mobile/i'    => 'Handheld Browser'
-                     );
-
-    foreach ($browser_array as $regex => $value)
-        if (preg_match($regex, $user_agent))
-            $browser = $value;
-
-    return $browser;
-}
-
+require_once "backend/api.php";
 
 $user_os        = getOS();
 $user_browser   = getBrowser();
+$os_family      = OSFamily($user_os);
 
-$device_details = "<strong>Browser: </strong>".$user_browser."<br /><strong>Operating System: </strong>".$user_os."";
-
-echo($device_details).'</br>';
-
-
-$path="C:/";
+if($os_family = "Windows"){
+    $path="C:/";
+} else {
+    $path="/";
+}
 if(isset($_GET["path"])){
     $path = urldecode($_GET["path"]);
 }
-echo $path."</br>";
 $dir = scandir($path);
 foreach($dir as $token)
 {
-    if(($token != ".") && ($token != ".."))
+    if($token != ".")
     {
         if(is_dir($path.'/'.$token))
         {
@@ -103,5 +36,5 @@ foreach($folders as $folder)
 foreach($files as $file)
 {
     $newpath = $path.'/'.$file;
-    echo '<a href = "fileHandler.php?file='.$file.'">'.$file.'</a></br>';
+    echo '<a href = "backend/fileHandler.php?file='.$file.'">'.$file.'</a></br>';
 }
